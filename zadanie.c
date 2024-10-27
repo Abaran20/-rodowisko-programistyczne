c85a6226beb7f4326c824b0e6470cf46b81e3f62
Abaran20 <Alexbaran64@gmail.com>
2024-10-27 10:01:15 +0100

    Update zadanie.c

--- zadanie.c
+++ zadanie.c
@@ -6,3 +6,25 @@ int main()
 
     return 0;
 }
+//zadanie 2
+
+
+
+
+
+
+//zadanie 3
+
+#include <stdio.h>
+int main(){
+    double num1,num2;
+    printf("podaj dwie liczby");
+    scanf("%lf %lf",&num1, &num2);
+    if (num1>num2)
+        printf("Wieksza liczba to : %.2lf\n",num1);
+    else if (num2>num1)
+        printf("Wieksza liczba to : %.2lf\n",num2);
+    else printf("obie liczby są rowne.\n");
+    return 0;
+}
+//zadanie 4
