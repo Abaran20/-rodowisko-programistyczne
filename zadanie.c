2a7913bf1ef55eff100395e303ec804e2529b7bc
Abaran20 <Alexbaran64@gmail.com>
2024-10-27 10:14:40 +0100

    Update zadanie.c

--- zadanie.c
+++ zadanie.c
@@ -28,3 +28,22 @@ int main(){
     return 0;
 }
 //zadanie 4
+#include <stdio.h>
+
+int main() {
+    double num1, num2, num3;
+    
+    printf("Podaj trzy liczby: ");
+    scanf("%lf %lf %lf", &num1, &num2, &num3);
+    
+    if (num1 > num2 && num1 > num3) {
+        printf("Największa liczba to %.2lf\n", num1);
+    } else if (num2 > num1 && num2 > num3) {
+        printf("Największa liczba to %.2lf\n", num2);
+    } else {
+        printf("Największa liczba to %.2lf\n", num3);
+    }
+    
+    return 0;
+}
+//zadanie 5
